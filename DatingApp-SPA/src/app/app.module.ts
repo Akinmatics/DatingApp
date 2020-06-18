6f4f445fe21cc16268bb870a78517f745aecda39
AKINMATICS <45751330+Akinmatics@users.noreply.github.com>
2020-06-18 04:42:42 +0100

    Enf of Section 6

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,6 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 import {FormsModule} from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+// import {ModalModule} from 'ngx-bootstrap/modal';
+
+
+
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -20,7 +26,10 @@ import { ErrorInterceptorProvider } from './_services/error.interceptor';
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      // ModalModule.forRoot(),
+      BsDropdownModule.forRoot(),
+      BrowserAnimationsModule
    ],
    providers: [
       AuthService,
--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,35 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-
   model: any = {};
 
-  constructor(private authService: AuthService) { }
+  constructor(
+    public authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  login() {
+    this.authService.login(this.model).subscribe(
+      (next) => {
+        this.alertify.success('Logged in Successfully');
+      },
+      error => {
+        this.alertify.error(error);
+      }
+    );
   }
 
-  login(){
-    this.authService.login(this.model).subscribe(next => {
-      console.log('Logged in Successfully');
-    }, error => {
-      console.log(error);
-    });
+  loggedIn() {
+    return this.authService.loggedIn();
   }
-
-  loggedIn(){
-    const token = localStorage.getItem('token');
-    return !!token;
-  }
-  logout(){
+  logout() {
     localStorage.removeItem('token');
-    console.log('Logged out');
+    this.alertify.message('Logged out');
   }
-
 }
